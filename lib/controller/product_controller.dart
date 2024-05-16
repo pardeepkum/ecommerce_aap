db9729610e5c7fdb0bbfd15e0d832f0dc62d989b
pardeepkumar <pk308914@gmail.com>
2024-05-16 09:41:48 +0530

    "search functinality added"

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -5,6 +5,7 @@ import '../services/api_services.dart';
 class ProductController extends GetxController {
   var isLoading = true.obs;
   var products = <Products>[].obs;
+  var filteredProducts = <Products>[].obs;
   var errorMessage = ''.obs;
 
   @override
@@ -19,7 +20,8 @@ class ProductController extends GetxController {
       var productsResult = await ApiService.fetchProducts();
       if (productsResult.isNotEmpty) {
         products.assignAll(productsResult);
-        Get.snackbar('Products Loaded', 'Here you go, you received ${productsResult.length} products. If you need more, just ask for it');
+        filteredProducts.assignAll(products); // Initialize filteredProducts with all products
+        Get.snackbar('Products Loaded', 'You received ${productsResult.length} products.');
       } else {
         errorMessage('No products found');
       }
@@ -27,6 +29,17 @@ class ProductController extends GetxController {
       errorMessage('Failed to load products: $e');
     } finally {
       isLoading(false);
+    }
+  }
+
+  // Method to filter products based on search query
+  void filterProducts(String query) {
+    if (query.isEmpty) {
+      // If query is empty, show all products
+      filteredProducts.assignAll(products);
+    } else {
+      // Filter products based on the search query (by name in this example)
+      filteredProducts.assignAll(products.where((product) => product.title!.toLowerCase().contains(query.toLowerCase())).toList());
     }
   }
 }
--- lib/screens/product_list.dart
+++ lib/screens/product_list.dart
@@ -5,43 +5,73 @@ import '../widgets/product_tile.dart';
 
 class ProductListScreen extends StatelessWidget {
   final ProductController productController = Get.put(ProductController());
+  final TextEditingController searchController = TextEditingController();
+
+  ProductListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Products'),
+        centerTitle: true,
+        title: const Text('Products'),
         actions: [
           IconButton(
-            icon: Icon(Icons.shopping_cart),
+            icon: const Icon(Icons.shopping_cart),
             onPressed: () {
               Get.toNamed('/cart');
             },
           )
         ],
       ),
-      body: Obx(() {
-        if (productController.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
-        } else if (productController.errorMessage.isNotEmpty) {
-          return Center(child: Text(productController.errorMessage.value));
-        } else {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 10),
-            child: ListView.builder(
-              itemCount: productController.products.length,
-              itemBuilder: (context, index) {
-                return ProductTile(
-                  product: productController.products[index],
-                  onTap: () {
-                    Get.toNamed('/product_detail', arguments: productController.products[index]);
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+
+                labelText: 'Search Products',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    searchController.clear();
+                    productController.filterProducts('');
                   },
-                );
+                ),
+              ),
+              onChanged: (value) {
+                productController.filterProducts(value);
               },
             ),
-          );
-        }
-      }),
+          ),
+          Expanded(
+            child: Obx(() {
+              if (productController.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (productController.errorMessage.isNotEmpty) {
+                return Center(child: Text(productController.errorMessage.value));
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: ListView.builder(
+                    itemCount: productController.filteredProducts.length,
+                    itemBuilder: (context, index) {
+                      return ProductTile(
+                        product: productController.filteredProducts[index],
+                        onTap: () {
+                          Get.toNamed('/product_detail', arguments: productController.filteredProducts[index]);
+                        },
+                      );
+                    },
+                  ),
+                );
+              }
+            }),
+          ),
+        ],
+      ),
     );
   }
 }
